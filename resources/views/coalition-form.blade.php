bc2b3bd590ebed6124ebf576bfd90eb44376a0f7
midnitedev101 <26995475+midnitedev101@users.noreply.github.com>
2022-03-29 13:14:58 -0700

    First push to coalition laravel assessment

---
+++ resources/views/coalition-form.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <form action="">
+        <div>
+            <label>Product Name</label><input type="text" class="product_name">
+        </div>
+        <div>
+            <label>Stock Quantity</label><input type="number" class="stock_quantity">
+        </div>
+        <div>
+            <label>Price Per Item</label><input type="number" class="price_per_item">
+        </div>
+        <input type="submit">
+    </form>
+    <script src="/js/app.js"></script>
+</body>
+</html>
